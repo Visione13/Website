c38d424a3b88a93845c882ebdbe9750ea6a634a9
Visione <marcel-gruenheid@gmx.de>
2024-07-22 00:23:58 +0200

    Init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+export default function Page() {
+
+    return (
+        <div className={'NavContainer'}>
+            test
+        </div>
+    )
+}
